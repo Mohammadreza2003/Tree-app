0893dcd13e93a682f24b2ea4023fdfebd96c66a8
Mohammadreza <75996851+Mohammadreza2003@users.noreply.github.com>
2024-01-17 18:48:09 +0330

    The app bugs was fixed

---
+++ src/data.js
@@ -0,0 +1,265 @@
+export const data = [
+    {
+        title: "خدمات",
+        children: [
+            {
+                title: "آموزش و تحقیقات",
+                children: [
+                    {
+                        title: "آموزش رسمی غیر دانشگاهی",
+                        children: [
+                            { title: "کودکستان غیر دولتی", link: "https://www.google.com/" },
+                            { title: "مدرسه غیر دولتی", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "آموزش رسمی دانشگاهی",
+                        children: [
+                            { title: "موسسه آموزش عالی آزاد", link: "https://www.google.com/" },
+                            { title: "مؤسسه آموزش عالی غیر دولتی (حضوری و مجازی)", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "آموزش مهارتی",
+                        children: [
+                            { title: "مرکز آموزش غیر دولتی (زبان، علمی و کنکور)", link: "https://www.google.com/" },
+                            { title: "تدریس خصوصی", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+            {
+                title: "سلامت",
+                children: [
+                    {
+                        title: "آموزش و ارتقا سلامت",
+                        children: [
+                            { title: "آموزشگاه بهداشت اصناف", link: "https://www.google.com/" },
+                            {
+                                title:
+                                    "مؤسسه خدمات آموزشی در امور فرآورده های آرایشی و بهداشتی", link: "https://www.google.com/"
+                            },
+                        ],
+                    },
+                    {
+                        title: "بهداشت",
+                        children: [
+                            { title: "دفتر خدمات سلامت", link: "https://www.google.com/" },
+                            { title: "خدمات بهداشت حرفه ای", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "درمان کلینیکی",
+                        children: [{ title: "بیمارستان", link: "" }, { title: "درمانگاه عمومی", link: "" }],
+                    },
+                ],
+            },
+            {
+                title: "امور اجتماعی و بهزیستی",
+                children: [
+                    {
+                        title: "نگهداری شبانه روزی کودکان بی سرپرست و خیابانی",
+                        children: [
+                            { title: "خانه نوباوگان", link: "https://www.google.com/" },
+                            { title: "خانه کودکان و نوجوانان (پسران 13 تا 18 سال)", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "سلامت دختران و زنان",
+                        children: [
+                            { title: "خانه امن زنان تحت خشونت خانگی", link: "https://www.google.com/" },
+                            {
+                                title:
+                                    "مرکز حمایت و توانمندسازی زنان آسیب دیده اجتماعی (راه نوین)", link: "https://www.google.com/"
+                            },
+                        ],
+                    },
+                    {
+                        title: "خدمات بهزیستی",
+                        children: [
+                            { title: "مجتمع خدمات بهزیستی", link: "https://www.google.com/" },
+                            { title: "مرکز مثبت زندگی", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: "کشاورزی",
+        children: [
+            {
+                title: "تولیدات کشاورزی",
+                children: [
+                    {
+                        title: "گلخانه",
+                        children: [
+                            { title: "شهرک گلخانه ای", link: "https://www.google.com/" },
+                            { title: "واحد گلخانه ای (مستقر در شهرک کشاورزی)", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "قارچ خوراکی",
+                        children: [
+                            { title: "شهرک قارچ خوراکی", link: "https://www.google.com/" },
+                            { title: "واحد قارچ خوراکی (مستقر در شهرک کشاورزی)", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "سایر محصولات",
+                        children: [
+                            { title: "پرورش خانگی گلمحمدی", link: "https://www.google.com/" },
+                            { title: "تولید خانگی آلوئه ورا", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+            {
+                title: "خدمات کشاورزی",
+                children: [
+                    {
+                        title: "حفظ نباتات و کنترل آفات",
+                        children: [
+                            {
+                                title:
+                                    "آزمایشگاه تشخیص آفات و بیماری های گیاهی (کلینیک های گیاه پزشکی)", link: "https://www.google.com/"
+                            },
+                            {
+                                title:
+                                    "تولید عوامل ماکرو ارگانیسم کنترل بیولوژیک آفات (انسکتاریوم گیاه پزشکی)", link: "https://www.google.com/"
+                            },
+                        ],
+                    },
+                    {
+                        title: "بذر و نهال",
+                        children: [
+                            { title: "واحد تولید نهال", link: "https://www.google.com/" },
+                            { title: "واحد تولید بذر اسپان قارچ خوراکی", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "مکانیزاسیون و ماشین‌آلات",
+                        children: [
+                            { title: "شرکت فنی تخصصی مکانیزاسیون", link: "https://www.google.com/" },
+                            { title: "خرده فروشی ادوات کشاورزی", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+            {
+                title: "تولیدات دامی",
+                children: [
+                    {
+                        title: "دام",
+                        children: [
+                            { title: "پرورش صنعتی و نیمه صنعتی گوسفند و بز", link: "https://www.google.com/" },
+                            { title: "پرورش صنعتی و نیمه صنعتی گاو و گاومیش", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "طیور",
+                        children: [
+                            { title: "پرورش صنعتی و نیمه صنعتی مرغ و طیور", link: "https://www.google.com/" },
+                            { title: "پرورش صنعتی و نیمه صنعتی جوجه یکروزه مرغ", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "شیلات",
+                        children: [
+                            { title: "صید و صیادی", link: "https://www.google.com/" },
+                            { title: "پرورش صنعتی و نیمه صنعتی ماهی در قفس", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        title: "صنعت",
+        children: [
+            {
+                title: "صنایع ماشین‌آلات و تجهیزات",
+                children: [
+                    {
+                        title: "ماشین آلات کشاورزی",
+                        children: [
+                            { title: "تولید قطعات و ماشین آلات کشاورزی", link: "https://www.google.com/" },
+                            { title: "تولید وتعمیر ابزار کشاورزی و باغبانی", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "ماشین آلات پرورش دام و شیلات",
+                        children: [
+                            { title: "تولید قطعات و ماشین آلات پرورش دام و شیلات", link: "https://www.google.com/" },
+                            { title: "تولید وتعمیر ماشین آلات مرغداری", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "ماشین آلات مهندسی، عمرانی و معدنی",
+                        children: [
+                            { title: "تولید قطعات و ماشین آلات پروژه عمرانی و معدنی", link: "https://www.google.com/" },
+                            { title: "تولید و تعمیر ماشین آلات راهسازی ساختمانی و معدنی", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+            {
+                title: "صنایع برقی و الکترونیکی",
+                children: [
+                    {
+                        title: "قطعات، لوازم جانبی و تجهیزات ارتباطی",
+                        children: [
+                            { title: "تولید قطعات، لوازم جانبی و تجهیزات ارتباطی", link: "https://www.google.com/" },
+                            {
+                                title:
+                                    "تولید قطعات گوشی تلفن، تلفن سانترال الکترونیکی و دیجیتالی", link: "https://www.google.com/"
+                            },
+                        ],
+                    },
+                    {
+                        title: "قطعات، لوازم جانبی و تجهیزات رایانه ای",
+                        children: [
+                            { title: "تولید قطعات، لوازم جانبی و تجهیزات رایانه ای", link: "https://www.google.com/" },
+                            { title: "تولید و مونتاژ تجهیزات جانبی رایانه", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "ابزار دقیق، اندازه گیری و ساعت",
+                        children: [
+                            { title: "تولید ابزار دقیق، اندازه گیری و ساعت", link: "https://www.google.com/" },
+                            { title: "تولید و تعمیر انواع ترازو", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+            {
+                title: "صنایع منسوجات و پوشاک",
+                children: [
+                    {
+                        title: "آماده‌سازی و ریسندگی الیاف نساجی",
+                        children: [
+                            {
+                                title: "آماده‌سازی و ریسندگی الیاف نساجی", link: "https://www.google.com/"
+                            },
+                            { title: "تولید و فروش انواع پنبه ،اسفنج ،الیاف پشم و کرک و پر", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "بافندگی منسوجات و تولید پارچه",
+                        children: [
+                            { title: "بافندگی منسوجات و تولید پارچه", link: "https://www.google.com/" },
+                            { title: "بافندگی انواع منسوجات به صورت ماشینی", link: "https://www.google.com/" },
+                        ],
+                    },
+                    {
+                        title: "تکمیل منسوجات",
+                        children: [
+                            { title: "تکمیل منسوجات", link: "https://www.google.com/" },
+                            { title: "پلیسه انواع لباس", link: "https://www.google.com/" },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+];
